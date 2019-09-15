5f2ac143e12f2c15c767e1d27530ecd48cca2c60
Greg Procunier <49077358+gprocunier@users.noreply.github.com>
2019-09-15 05:28:00 -0400

    Update generate-certs-freeipa.sh

--- generate-certs-freeipa.sh
+++ generate-certs-freeipa.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # Create Key/CSR/CRT signed by FreeIPA
-# gprocunier@symcor.com [08-03-2019]
+# greg.procunier@gmail.com [08-03-2019]
 #
 # hero mode for those times you can't go all-in with freeipa+novajoin 
 # but still want public SSL endpoints
 
 declare -A config
-config["overcloudHost"]="overcloud.idm.symrad.com"
-config["ipaRealm"]="IDM.SYMRAD.COM"
-config["keyLocation"]="/home/stack/ssl-build/overcloud.idm.symrad.com.key.pem"
-config["csrLocation"]="/home/stack/ssl-build/overcloud.idm.symrad.com.csr.pem"
-config["crtLocation"]="/home/stack/ssl-build/overcloud.idm.symrad.com.crt.pem"
+config["overcloudHost"]="overcloud.idm.example.com"
+config["ipaRealm"]="IDM.EXAMPLE.COM"
+config["keyLocation"]="/home/stack/ssl-build/overcloud.idm.example.com.key.pem"
+config["csrLocation"]="/home/stack/ssl-build/overcloud.idm.example.com.csr.pem"
+config["crtLocation"]="/home/stack/ssl-build/overcloud.idm.example.com.crt.pem"
 config["chownTarget"]="stack:stack"
 config["keyBits"]="2048"
 config["openssl_cnf"]="/etc/pki/tls/openssl.cnf"
 config["ipaAdmin"]="admin"
 
 declare -a sanHosts
-sanHosts[0]='DNS:overcloud.idm.symrad.com'
-sanHosts[1]='DNS:overcloud.ctlplane.idm.symrad.com'
-sanHosts[2]='DNS:overcloud.internalapi.idm.symrad.com'
-sanHosts[3]='DNS:overcloud.storage.idm.symrad.com'
-sanHosts[4]='DNS:overcloud.storagemgmt.idm.symrad.com'
-sanHosts[5]='DNS:overcloud.storagenfs.idm.symrad.com'
+sanHosts[0]='DNS:overcloud.idm.example.com'
+sanHosts[1]='DNS:overcloud.ctlplane.idm.example.com'
+sanHosts[2]='DNS:overcloud.internalapi.idm.example.com'
+sanHosts[3]='DNS:overcloud.storage.idm.example.com'
+sanHosts[4]='DNS:overcloud.storagemgmt.idm.example.com'
+sanHosts[5]='DNS:overcloud.storagenfs.idm.example.com'
 
 
 ### Main
